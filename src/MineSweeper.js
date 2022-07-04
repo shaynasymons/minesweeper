b70946006eb2aefb6af36096de71b3ee6f2cd087
Shayna Symons <shaynasymons@gmail.com>
2022-07-04 19:25:18 -0400

    exercise-0
    Exercise 0
    ----------
    Hello World. Create a ReactJS app that can be run, and does nothing more than display "Hello World" on screen.
    Add a README.md file that tells me how to run the app.
    Add a package.json file with any and all dependencies needed to build and run the project.

---
+++ src/MineSweeper.js
@@ -0,0 +1,13 @@
+import './MineSweeper.css';
+
+function MineSweeper() {
+  return (
+    <div className="mine-sweeper">
+      <header className="mine-sweeper-header">
+        <p>Hello world. </p>
+      </header>
+    </div>
+  );
+}
+
+export default MineSweeper;
